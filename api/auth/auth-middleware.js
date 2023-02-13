e6cc404ba85964e37afee9a9114f6dddfbcd352d
tugce <tugceozdemiir@hotmail.com>
2023-02-13 17:46:12 +0300

    devam

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,6 +1,4 @@
-const Users =require("../users/users-model");
-
-
+const Users = require("../users/users-model");
 
 /*
   Kullanıcının sunucuda kayıtlı bir oturumu yoksa
@@ -10,8 +8,12 @@ const Users =require("../users/users-model");
     "mesaj": "Geçemezsiniz!"
   }
 */
-function sinirli(req,res,next) {
-next()
+function sinirli(req, res, next) {
+  try {
+    next();
+  } catch (err) {
+    next(err);
+  }
 }
 
 /*
@@ -22,8 +24,17 @@ next()
     "mesaj": "Username kullaniliyor"
   }
 */
-function usernameBostami(req,res,next) {
-  next()
+async function usernameBostami(req, res, next) {
+  try {
+    const present = await Users.goreBul({ username: req.body.username }); //user var= =>[{user}],  user yok=> []
+    if (present.length > 0) {
+      next({ status: 422, message: "Username kullaniliyor" });
+    } else {
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
 }
 
 /*
@@ -34,8 +45,17 @@ function usernameBostami(req,res,next) {
     "mesaj": "Geçersiz kriter"
   }
 */
-function usernameVarmi(req,res,next) {
-  next()
+async function usernameVarmi(req, res, next) {
+  try {
+    const present = await Users.goreBul({ username: req.body.username });
+    if (!present.length > 0) {
+      next({ status: 401, message: "Geçersiz kriter" });
+    } else {
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
 }
 
 /*
@@ -46,13 +66,18 @@ function usernameVarmi(req,res,next) {
     "mesaj": "Şifre 3 karakterden fazla olmalı"
   }
 */
-function sifreGecerlimi(req,res,next) {
-  next()
+function sifreGecerlimi(req, res, next) {
+  try {
+    const { password } = req.body;
+    if (!password || password.length <= 3) {
+      next({ status: 422, message: "Şifre 3 karakterden fazla olmalı" });
+    } else {
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
 }
 
 // Diğer modüllerde kullanılabilmesi için fonksiyonları "exports" nesnesine eklemeyi unutmayın.
-module.exports={sinirli,
-  usernameBostami,
-  usernameVarmi,
-  sifreGecerlimi
-}
+module.exports = { sinirli, usernameBostami, usernameVarmi, sifreGecerlimi };
